1a307086e4fe5246c423c79bce3d1f82c48a3f8b
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-03-19 06:58:40 +0000

    Basic pygame shell

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((600, 400))
+
+x, y = 300, 200
+
+running = True
+while running:
+    pygame.time.delay(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
+    pygame.display.update()
+
+pygame.quit()
