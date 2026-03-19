199f13bf7eca73f2098e118856ed6609bf37655f
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-03-19 07:15:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import pygame
 
 pygame.init()
 win = pygame.display.set_mode((600, 400))
+def do(genomes, config):
+    x, y = 300, 200
 
-x, y = 300, 200
+    running = True
+    while running:
+        pygame.time.delay(30)
 
-running = True
-while running:
-    pygame.time.delay(30)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
     
-    win.fill((0,0,0))
-    inputs = (x/600, y/400)
-    output = net.activate(inputs)
-    x += output[0]
-    y += output[1]
+        win.fill((0,0,0))
+        inputs = (x/600, y/400)
+        output = net.activate(inputs)
+        x += output[0] * 10
+     
+        y += output[1] * 10
         
-    pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
-    pygame.display.update()
+        pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
+        pygame.display.update()
 
 pygame.quit()
