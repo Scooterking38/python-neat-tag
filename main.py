acd243efd067622fa80f4064296ad2023d452796
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-03-19 07:06:37 +0000

    Activate neural net IO

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     win.fill((0,0,0))
+    inputs = (x, y)
+    output = net.activate(inputs)
+    x += output[0]
+    y += output[1]
+        
     pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
     pygame.display.update()
 
