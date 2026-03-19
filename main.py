bb4af296fc1f844200c8ee06e7ad469e94a37dde
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-03-19 07:37:33 +0000

    Add net definition and genome parsing

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import pygame
 pygame.init()
 win = pygame.display.set_mode((600, 400))
 def do(genomes, config):
-    x, y = 300, 200
+    for _, genome in genomes:
+        x, y = 300, 200
 
-    running = True
-    while running:
-        pygame.time.delay(30)
+        running = True
+        while running:
+            pygame.time.delay(30)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
     
-        win.fill((0,0,0))
-        inputs = (x/600, y/400)
-        output = net.activate(inputs)
-        x += output[0] * 10
+            win.fill((0,0,0))
+            inputs = (x/600, y/400)
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            output = net.activate(inputs)
+            x += output[0] * 10
      
-        y += output[1] * 10
+            y += output[1] * 10
         
-        pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
-        pygame.display.update()
+            pygame.draw.rect(win, (255,0,0), (x, y, 20, 20))
+            pygame.display.update()
 
 pygame.quit()
