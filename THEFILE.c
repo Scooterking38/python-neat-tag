826fc1f99a47f8a7283c8937bc83be0f6ac6245c
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-06-30 12:33:27 +0100

    Update THEFILE.c

--- THEFILE.c
+++ THEFILE.c
@@ -10,23 +10,41 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <string.h>
 #include <cs50.h>
 
-int valid(long number)
+int company(long number)
 {
-    if (number % 2 == 0) {
-        long i = number
-        int length = 0
-        while (i > 0) {
-            i = i / 10
-            length++
-        }
-        
+
+    long i = number;
+    int length = 0;
+    while (i > 0) {
+        i = i / 10;
+        length++;
     }
+    long x = number;
+    while (x > 99) {
+        x = x / 10;
+    }
+    
+    if (length == 15 && (x == 34 || x == 37)) {
+        printf("AMERICAN EXPRESS");
+    }
+    if (length == 16 && (x == 51 || x == 52 || x == 53 || x == 54 || x == 55)) {
+        printf("MASTERCARD");
+    }    
+    if ((length == 13 || length == 16) && (x > 39 && x < 50))
+        printf("VISA");
     return 0;
 }
 
-int company()
+int valid(long number)
 {
-    
+    long i = number;
+    int length = 0;
+    while (i > 0) {
+        i = i / 10;
+        length++;
+    }
+    if (length)
+    return 0;
 }
 
 int main()
